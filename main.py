c7f462c61621649bdae0b6e6b7679db23ef63f20
sinryan137 <sineadryan137@gmail.com>
2021-07-27 14:40:54 +0100

    Get APIs

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import requests
+
+data = requests.get("http://api.open-notify.org/iss-now.json")
+parsed_data = data.json()
+print(parsed_data["timestamp"])
+
+data1 = requests.get(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=8HKU2KODGW7DY1OA")
+print(data1)
+
